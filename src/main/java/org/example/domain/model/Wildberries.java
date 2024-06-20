1aa8d1bf77761739361401b0d84ec21ace5bb937
athebyme <bruhov37@mail.ru>
2024-06-20 20:16:33 +0300

    Revert "20.06 update"
    This reverts commit 92b841b3f7cc63846d530421a85f7fdac9f975eb.

--- src/main/java/org/example/domain/model/Wildberries.java
+++
@@ -1,9 +0,0 @@
-package org.example.domain.model;
-
-import org.example.domain.model.abstractions.Marketplace;
-
-public class Wildberries extends Marketplace {
-    public Wildberries(int id, String name) {
-        super(id, name);
-    }
-}
